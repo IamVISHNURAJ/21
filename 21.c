8296cbb186e8f1d828b50d22cc993e19b6a61abf
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 17:36:47 +0530

    Update 21.c

--- 21.c
+++ 21.c
@@ -2,8 +2,8 @@
 int main()
 {
     int a,b,c,sum;
-    scanf("%d%d%d",&c,&a,&b);
-    sum=(c*(2*a+(c-1)*b))/2;
+    scanf("%d%d%d",&a,&b,&c);
+    sum=(a*(2*b+(a-1)*c))/2;
     printf("%d ",sum);
 return 0;
 }
